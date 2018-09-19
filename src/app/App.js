21dcb1b88a2e3392cd8c05b4a799517526b5e05c
Sergey K <s.kalmykov@specdep.ru>
2018-09-19 12:52:47 +0300

    11kg of sheetcode

--- src/app/App.js
+++ src/app/App.js
@@ -23,38 +23,19 @@ class App extends Component {
     });
 
     let deferredPrompt;
-
     window.addEventListener('beforeinstallprompt', (e) => {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
       // Update UI notify the user they can add to home screen
-      btnAdd.style.display = 'block';
+      console.log('add btn show');
       console.log('Хуй! Хуй! Хуй!');
     });
 
-    btnAdd.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      btnAdd.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice
-        .then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-    });
     window.addEventListener('appinstalled', (evt) => {
-      app.logEvent('a2hs', 'installed');
+      console.log('a2hs', 'installed');
     });
-
-
 
   }
 
